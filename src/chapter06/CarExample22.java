cecf71b1c4818c3d5eda9ff1660cbd7e12764883
YenuPark <yenuzzang@naver.com>
2020-11-13 00:38:46 +0900

    instance 멤버와 this

---
+++ src/chapter06/CarExample22.java
@@ -0,0 +1,15 @@
+package chapter06;
+
+public class CarExample22 {
+
+    public static void main(String[] args) {
+            Car2 myCar = new Car2("포르쉐");
+            Car2 yourCar = new Car2("벤츠");
+
+            myCar.run();
+            yourCar.run();
+    }
+
+
+
+}
