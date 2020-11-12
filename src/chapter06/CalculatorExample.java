4f3e92b2dd2bd15d76625ebf9a71035b926aeac9
YenuPark <yenuzzang@naver.com>
2020-11-13 00:43:50 +0900

    calculator

--- src/chapter06/CalculatorExample.java
+++ src/chapter06/CalculatorExample.java
@@ -3,11 +3,13 @@ package chapter06;
 public class CalculatorExample {
 
     public static void main(String[] args) {
+
         Carculator myCalcu = new Carculator();
 
         double result1 = myCalcu.areaRectangle(10);
         double result2 = myCalcu.areaRectangle(10, 20);
         System.out.println(result1);
         System.out.println(result2);
+
     }
 }
